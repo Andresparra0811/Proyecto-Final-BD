322db972b07ce42eb1d023ea9cb80fabbd0441ea
Andres Parra <110069461+Andresparra0811@users.noreply.github.com>
2022-11-21 11:10:57 -0500

    Cambios de texto

--- Proyecto Final Supermercado/frmPrincipal_Usuario.cs
+++ Proyecto Final Supermercado/frmPrincipal_Usuario.cs
@@ -91,7 +91,7 @@ namespace Proyecto_Final_Supermercado
         {
             if (txtID.Text == "0")
             {
-                if (MessageBox.Show("¿Deseas registrar el libro " + txtnombre.Text + "?", "Mensaje",
+                if (MessageBox.Show("¿Deseas registrar el producto " + txtnombre.Text + "?", "Mensaje",
                     MessageBoxButtons.YesNo, MessageBoxIcon.Information) == System.Windows.Forms.DialogResult.Yes)
                 {
 
@@ -151,7 +151,7 @@ namespace Proyecto_Final_Supermercado
         {
             if (txtID.Text != "")
             {
-                if (MessageBox.Show("¿Deseas modificar el libro " + txtnombre.Text + "?", "Mensaje",
+                if (MessageBox.Show("¿Deseas modificar el producto " + txtnombre.Text + "?", "Mensaje",
                     MessageBoxButtons.YesNo, MessageBoxIcon.Information) == System.Windows.Forms.DialogResult.Yes)
                 {
 
@@ -167,7 +167,7 @@ namespace Proyecto_Final_Supermercado
         {
             if (txtID.Text != "")
             {
-                if (MessageBox.Show("¿Deseas eliminar el libro " + txtnombre.Text + "?", "Mensaje",
+                if (MessageBox.Show("¿Deseas eliminar el producto " + txtnombre.Text + "?", "Mensaje",
                     MessageBoxButtons.YesNo, MessageBoxIcon.Information) == System.Windows.Forms.DialogResult.Yes)
                 {
 
