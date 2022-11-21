0bb630b6cfa944124cc7fbd635b9748da4fe1307
Andres Parra <andresparrag0811@gmail.com>
2022-11-21 00:21:45 -0500

    Capa Presentacion

--- Capa_presentacion/ClasePresentacion.cs
+++ Capa_presentacion/ClasePresentacion.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Capa_Datos;
+using Capa_Logica;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Capa_presentacion
 {
-    public class Class1
+    public class ClasePresentacion
     {
+        ClaseDatos objd = new ClaseDatos();
+
+        public DataTable N_listar_libros()
+        {
+            return objd.D_listar_productos();
+
+        }
+        public DataTable N_buscar_producto(ClaseLogica obje)
+        {
+            return objd.D_buscar_producto(obje);
+
+        }
+        public String N_CUD(ClaseLogica obje)
+        {
+            return objd.D_CUD(obje);
+
+        }
     }
 }
